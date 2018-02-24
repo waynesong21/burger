378e485cbc0237c2d4081c44d135cfab060be0b4
waynesong21 <waynesong21@gmail.com>
2018-02-24 10:10:48 -0800

    not finished

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", true);
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(err, res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
